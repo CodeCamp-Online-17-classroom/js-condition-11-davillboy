706e4df014e3549a59cd4dbd429ccefa4e1ec0ed
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 09:42:47 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // write code here
+<script>
+        let user = prompt("Enter your username:");
+        
+        if (!user || user.trim() === "") {
+        user = "guest";
+        } else if (user === "codecamp") {
+        let password = prompt("Enter your password:");
+        if (password === "123456") {
+            user = "codecamp"; 
+        } else {
+            alert("Wrong password");
+            user = "guest"; 
+        }
+        } else {
+        user = "guest";
+        }
+
+        alert("Welcome, " + user);
+
+    </script>
